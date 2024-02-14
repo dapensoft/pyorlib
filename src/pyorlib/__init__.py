9568833ab1a8383b769c989bdbd9ae2ee2e72376
Manuel Da Pena <65864237+mdapena@users.noreply.github.com>
2024-02-14 00:24:14 -0400

    Update pyproject.toml config

--- src/pyorlib/__init__.py
+++ src/pyorlib/__init__.py
@@ -1,5 +1,8 @@
-"""A Python Operations Research Library"""
+"""
+A powerful Python library for operations research. Define, solve, and interact with
+mathematical models in a standardized manner across different optimization packages.
+"""
 
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 
 __all__ = ()
