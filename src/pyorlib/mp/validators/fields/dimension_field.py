d2907b3061d97d785663e402637e479e504701da
Manuel Da Pena <65864237+mdapena@users.noreply.github.com>
2024-02-18 18:30:27 -0400

    Fix DimensionField Descriptor and Tests Coverage

--- src/pyorlib/mp/validators/fields/dimension_field.py
+++ src/pyorlib/mp/validators/fields/dimension_field.py
@@ -17,7 +17,7 @@ class DimensionField(FieldValidator[int]):
     __slots__ = ["_min", "_max"]
 
     @property
-    def min(self) -> float | None:
+    def min(self) -> int | None:
         """
         Get the minimum value allowed.
         :return: The minimum value or None if not set.
@@ -25,39 +25,48 @@ class DimensionField(FieldValidator[int]):
         return self._min
 
     @property
-    def max(self) -> float | None:
+    def max(self) -> int | None:
         """
         Get the maximum value allowed.
         :return: The maximum value or None if not set.
         """
         return self._max
 
-    def __init__(self, min: float | None = None, max: float | None = None):
+    def __init__(self, min: int | None = None, max: int | None = None):
         """
         Initialize a new DimensionField instance.
         :param min: The minimum value allowed for the dimension field. Defaults to None.
         :param max: The maximum value allowed for the dimension field. Defaults to None.
         """
+        # Calls the base init method
         super().__init__()
-        self._min: float | None = min
-        self._max: float | None = max
 
-        # Validations
-        if self.min is not None and self.min < 1:
-            raise ValueError("Dimension field minimum must be greater than or equal to 1.")
+        # Applies validations
+        if min is not None and not ValueTypeValidator.is_integer(min):
+            raise ValueError(f"{self._public_name} 'min' value must be an integer number.")
 
-        if self.max is not None and self.min is not None and self.min > self.max:
-            raise ValueError("Dimension field minimum cannot be greater than maximum.")
+        if max is not None and not ValueTypeValidator.is_integer(max):
+            raise ValueError(f"{self._public_name} 'max' value must be an integer number.")
+
+        if min is not None and min < 0:
+            raise ValueError(f"{self._public_name} 'min' value must be greater than or equal to 0.")
+
+        if max is not None and min is not None and min > max:
+            raise ValueError(f"{self._public_name} 'min' value cannot be greater than maximum.")
+
+        # Instance attributes
+        self._min: int | None = min
+        self._max: int | None = max
 
     def validate(self, value: int | None) -> None:
         if value is None:
-            raise ValueError(f"{self._public_name} cannot be None")
-
-        if self._min is not None and value < self._min:
-            raise ValueError(f"{self._public_name} value must be greater than or equal to {self._min}")
-
-        if self._max is not None and value > self._max:
-            raise ValueError(f"{self._public_name} value must be less than or equal to {self._max}")
+            raise ValueError(f"{self._public_name} value cannot be None.")
 
         if not ValueTypeValidator.is_integer(value):
-            raise ValueError(f"{self._public_name} value must be a valid integer number")
+            raise ValueError(f"{self._public_name} value must be an integer number.")
+
+        if self._min is not None and value < self._min:
+            raise ValueError(f"{self._public_name} value must be greater than or equal to {self._min}.")
+
+        if self._max is not None and value > self._max:
+            raise ValueError(f"{self._public_name} value must be less than or equal to {self._max}.")
--- src/pyorlib/mp/validators/value_type_validator.py
+++ src/pyorlib/mp/validators/value_type_validator.py
@@ -4,23 +4,24 @@ from math import inf
 class ValueTypeValidator:
     """
     This class has two static methods to validate if a
-    given "float" number is a binary or integer number.
+    given "float" or "int" number is a valid binary or
+    integer number.
     """
 
     @staticmethod
-    def is_binary(num: float) -> bool:
+    def is_binary(num: float | int) -> bool:
         """
-        This method checks if the given float number is binary or not.
-        :param num: float number to be checked
-        :return: True if the given float number is binary else False
+        This method checks if the given float/int number is binary or not.
+        :param num: float/int number to be checked
+        :return: True if the given float/int number is binary else False
         """
         return num is not None and num in [0, 1]
 
     @staticmethod
-    def is_integer(num: float) -> bool:
+    def is_integer(num: float | int) -> bool:
         """
-        This method checks if the given float number is an integer or not.
-        :param num: float number to be checked
-        :return: True if the given float number is an integer else False
+        This method checks if the given float/int number is a valid integer or not.
+        :param num: float/int number to be checked
+        :return: True if the given float/int number is an integer else False
         """
-        return num is not None and (num == inf or num == -inf or float(num).is_integer())
+        return num is not None and (num == inf or num == -inf or isinstance(num, int) or float(num).is_integer())
