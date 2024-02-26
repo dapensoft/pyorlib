25aa312adc71f485c5d14b0f6fb01b4cd5f76167
Manuel Da Pena <65864237+mdapena@users.noreply.github.com>
2024-02-26 19:32:04 -0400

    Add definitions & parameters module description

--- src/pyorlib/structures/parameters/__init__.py
+++ src/pyorlib/structures/parameters/__init__.py
@@ -1,5 +1,7 @@
 """
-Parameters module description...
+The Parameters module in PyORlib provides a set of classes to handle parameters within an optimization model. These
+classes offer a structured and modular approach to manage different types of parameters, their values, validations,
+and associated properties.
 """
 
 from .multi_value_parameter import MultiValueParameter
